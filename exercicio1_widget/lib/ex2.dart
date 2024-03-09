81777cca5ecf118ab9c0f114cf0c94d5c8c22196
EllysonRPT <reptile1284@gmail.com>
2024-03-08 21:18:51 -0300

    08/03

--- exercicio1_widget/lib/ex2.dart
+++ exercicio1_widget/lib/ex2.dart
@@ -1,95 +1,39 @@
+import 'package:exercicio1_widget/Exercicio1.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Exemplo de Interface'),
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: MyWidget(),
-        ),
-      ),
+        appBar:AppBar(title: Text("Exercicio2")) ,
+        body: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+            Column(
+              children: [
+                Text('Texto da Primeira Coluna')
+              ],
+            ),
+            Column(
+              children: [
+                Text('Texto da Segunda Coluna')
+              ],
+            ),
+            Column(
+              children: [
+                Text('Texto da Terceira Coluna')
+              ],
+
+            ),
+          ],),
+        )),
     );
-  }
+  }  
 }
 
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Organização Horizontal',
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
-        SizedBox(height: 10.0),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(
-              Icons.home,
-              size: 50.0,
-              color: Colors.blue,
-            ),
-            Text(
-              'Casa',
-              style: TextStyle(fontSize: 18.0),
-            ),
-            Image.network(
-              'img/foto1.jpg',
-              width: 50.0,
-              height: 50.0,
-            ),
-          ],
-        ),
-        SizedBox(height: 20.0),
-        Text(
-          'Organização Vertical',
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
-        
-        SizedBox(height: 10.0),
-        Column(
-          children: [
-            Row(
-              children: [
-                Icon(
-                  Icons.school,
-                  size: 50.0,
-                  color: Colors.green,
-                ),
-                Text(
-                  'Escola',
-                  style: TextStyle(fontSize: 18.0),
-                ),
-              ],
-            ),
-            SizedBox(height: 10.0),
-            Row(
-              children: [
-                Icon(
-                  Icons.work,
-                  size: 50.0,
-                  color: Colors.orange,
-                ),
-                Text(
-                  'Trabalho',
-                  style: TextStyle(fontSize: 18.0),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ],
-    );
-  }
-}
--- exercicio1_widget/lib/ex6.dart
+++ exercicio1_widget/lib/ex6.dart
@@ -15,56 +15,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Center(
           child: Container(
-            width:300,
-            height:300,
-            color: Colors.blue,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Implemente ação do botão aqui
-                  },
-                  child: Text(
-                    'Botão',
-                    style: TextStyle(
-                      color: Color(0xCD000000),
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  width: 150,
-                  height: 50,
-                  color: Colors.red,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Digite aqui',
-                      hintStyle: TextStyle(color: Colors.white),
-                    ),
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-                SizedBox(height: 10),
-                Container(
-                  width: 150,
-                  height: 100,
-                  color: Colors.green,
-                  child: Center(
-                    child: Text(
-                      'quer ajuda?',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
+            row(MainAxisAlignment.spaceEvenly,children[Text('Container Example')])
           ),
         ),
       ),
